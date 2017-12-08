0b1cad4bba6f3349bcc98abdf92f02bd404a62be
Johann Hospice <hospice@informatique.univ-paris-diderot.fr>
2017-12-08 11:33:46 +0100

    Utilisation du bon capteur couleur HiTechnic

--- src/mindstorm/tools/Engine.java
+++ src/mindstorm/tools/Engine.java
@@ -1,30 +1,21 @@
 package mindstorm.tools;
 
-import lejos.hardware.ev3.EV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.HiTechnicColorSensor;
 
 /**
  * Contient tous les composants moteurs du mindstorm
  */
 public class Engine {
-    private static final String PORT_COLOR = "S1";
-    private static final String PORT_MOTOR_RIGHT = "A";
-    private static final String PORT_MOTOR_LEFT = "B";
-
     private final EV3LargeRegulatedMotor lMotor;
     private final EV3LargeRegulatedMotor rMotor;
-    private final ColorSensorHandler cSensorHandler;
+    private HiTechnicColorSensor colorSensor;
 
-    public Engine(EV3 ev3) {
-        lMotor = new EV3LargeRegulatedMotor(ev3.getPort(PORT_MOTOR_RIGHT));
-        rMotor = new EV3LargeRegulatedMotor(ev3.getPort(PORT_MOTOR_LEFT));
-        cSensorHandler = new ColorSensorHandler(new EV3ColorSensor(ev3.getPort(PORT_COLOR)));
-    }
-
-
-    public ColorSensorHandler getColorSensorHandler() {
-        return cSensorHandler;
+    public Engine(Port colorSensorPort, Port leftMotorPort, Port rightMotorPort) {
+        lMotor = new EV3LargeRegulatedMotor(leftMotorPort);
+        rMotor = new EV3LargeRegulatedMotor(rightMotorPort);
+        colorSensor = new HiTechnicColorSensor(colorSensorPort);
     }
 
     public EV3LargeRegulatedMotor getLeftMotor() {
@@ -35,10 +26,13 @@ public class Engine {
         return rMotor;
     }
 
+    public void close() {
+        lMotor.close();
+        rMotor.close();
+        colorSensor.close();
+    }
 
-	public void close() {
-		lMotor.close();
-		rMotor.close();
-		cSensorHandler.close();
-	}
+    public HiTechnicColorSensor getColorSensor() {
+        return colorSensor;
+    }
 }
